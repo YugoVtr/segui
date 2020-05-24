2414eba8cfce45579b8ffd8e6dff20b6e560abcc
mariaregina <santosdedeusm@gmail.com>
2020-05-24 19:33:45 -0300

    Tela de login

---
+++ Components/login/Login.js
@@ -0,0 +1,39 @@
+import React, { Fragment } from 'react';
+import { Text, View, Image, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import Estilo from './Estilo.js';
+
+const Login = () =>{
+    return (
+        <Fragment>
+        <View>
+            <ImageBackground style={Estilo.backgroundImage} source={require("./imagens/ponta.png")} >
+            </ImageBackground >
+        </View>
+        <View >
+            <Image style={Estilo.seta1} source={require("./imagens/seta.png")} />
+            <Image style={Estilo.contorno} source={require("./imagens/borda.png")} />
+        </View>
+            <Image style={Estilo.logo} source={require("./imagens/logo_segui.jpg")} />
+            <Image style={Estilo.seta2} source={require("./imagens/seta.png")} />
+        <View style={Estilo.container}>
+            <View>
+                <Text style={Estilo.login}>Fazer login</Text>
+                <TextInput
+                    style={Estilo.input}
+                    placeholder="insira seu número de telefone"
+                />
+                <TextInput
+                    style={ Estilo.input}
+                    secureTextEntry={true}
+                    placeholder="senha"
+                />
+                <TouchableOpacity style={Estilo.botao}>
+                    <Text style={Estilo.texto}>login</Text>
+                </TouchableOpacity>
+            </View>
+      </View>
+  </Fragment>
+    )
+}
+
+export default Login;
